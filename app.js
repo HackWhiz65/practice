69805d1eefd2d9481d09dabfd7677a7b800430bf
Himesh <pandeyhimesh65@gmail.com>
2026-02-08 02:31:38 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,108 @@
+// All Javascript Required for React JS:
+
+// 1.Arrow Functions
+// 2.Ternary Operators
+// 3.Objects
+// 4.Map, Filter, reduce..
+// 5.Async-Await, Promises, Fetch
+// 6. What is DOM?
+// 7.Import/Export Notation
+// 8.Ternary operators and conditionals
+// 9.Optional Chaining
+// 10.Template Literals
+
+// arrow function :
+//      (parameter) => some code
+
+// normal func:
+// const hello = function(){
+//     console.log("Hello");
+// }
+// hello();
+
+// arrow func:
+
+// const hello = () => console.log("Hello");
+// hello();
+
+// const hello2 = (name) => {
+//     console.log(`Hi ${name}! I'm here.`)
+// }
+// hello2("Himesh");
+
+// example2:
+
+// setTimeout(callback, 3000);
+// setTimeout(hello, 3000);
+
+// function hello(){
+//     console.log(`Hyyyy`);
+// }
+
+// const numbers = [1,2,3,4,5,6];
+
+// const squares = numbers.map((element) => Math.pow(element,2));
+// console.log(squares);
+
+// function sum(a,b){
+//     return a + b
+// }
+// result = sum(3,5);
+
+// console.log(result);
+
+// const func =(a,b)=> {
+//     return a + b
+// }
+// result = func(5,7);
+
+// console.log(`The sum is: ${result}.`);
+
+// loops in js:
+// for(let i=0; i<=5; i++){
+//     console.log("Hello Ultra!");
+// }
+
+// b = 5;
+// b++;
+// console.log(b);
+// console.log(++b);
+
+// age = 24;
+// how = "young";
+// console.log(`My age is ${age} and I'm ${how}.`);
+
+// operators:
+// ++a = preincrement
+// a++ = post increment
+// a**b = a ko power b
+
+
+// let a = 7;
+// let b = 5;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(Math.floor(a/b));
+// console.log(a%b);
+// console.log(++a);
+// console.log(a++);
+// console.log(a**2);
+
+// datatype and type of(a):
+// typeof(a) shows datatype of a.
+
+// let a = 1.8762365;
+// console.log(typeof(a));
+
+// = is assignment operator
+//    == : comparision operator
+//    === : strictly equality operator. compares both value and type
+//  
+
+// a = 5;
+// b = '5';
+
+// console.log(a==b);
+// console.log(a===b);
